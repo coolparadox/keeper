80f5325dc2c86a2ba17aeb3107bb5dc1255de464
Rafael Lorandi <coolparadox@gmail.com>
2023-07-05 10:24:10 +0100

    recover --force --verbose

--- src/recover.sh
+++ src/recover.sh
@@ -4,7 +4,16 @@ ME=$(basename $0)
 fail() { echo "${ME}: error: $*" >&2 ; exit 1 ; }
 test -v KEEPDB || fail "missing KEEPDB environment variable"
 test -d "$KEEPDB" || fail "missing KEEPDB directory '$KEEPDB'"
-usage() { echo "usage: $ME <OID..."; exit 1; }
+usage() { echo "usage: $ME [-f|--force] [-v|--verbose] <OID..."; exit 1; }
+IS_FORCE=false
+IS_QUIET=true
+while test $# -ne 0 ; do
+    case $1 in
+        -f|--force) shift ; IS_FORCE=true ;;
+        -v|--verbose) shift ; IS_QUIET=false ;;
+        *) usage ;;
+    esac
+done
 test $# -eq 0 || usage
 sed 's/  */\n/g' | \
 while read OID ; do
@@ -12,7 +21,11 @@ while read OID ; do
     echo $OID | tr '/' '\n' | sed '1s/./&\//g' | tr -d '\n'
     echo
 done | while read PREFIX ; do
-    FILENAME=$(sed -e '/^filename:/!d' -e 's/^[^:]*://' "${PREFIX}.label")
-    test -e "$FILENAME" && fail "filename '$FILENAME' already exists" || :
+    LABEL_FILE="${PREFIX}.label"
+    FILENAME=$(sed -e '/^filename:/!d' -e 's/^[^:]*://' "$LABEL_FILE")
+    test -n "$FILENAME" || fail "missing 'filename:...' in $(basename $LABEL_FILE)"
+    $IS_FORCE || { test -e "$FILENAME" && fail "filename '$FILENAME' already exists" || : ; }
+    $IS_QUIET || echo -n $(basename "$PREFIX")\ --\>\  >&2
     gzip -cd "${PREFIX}.gz" >$FILENAME
+    $IS_QUIET || echo $FILENAME >&2
 done
