0409e7dec7f8e9d37a33bbc3e0dd10a4d1bf1209
Rafael Lorandi <coolparadox@gmail.com>
2023-07-05 16:25:26 +0100

    warn on overwriting file during recovery

--- src/recover.sh
+++ src/recover.sh
@@ -4,28 +4,28 @@ ME=$(basename $0)
 fail() { echo "${ME}: error: $*" >&2 ; exit 1 ; }
 test -v KEEPDB || fail "missing KEEPDB environment variable"
 test -d "$KEEPDB" || fail "missing KEEPDB directory '$KEEPDB'"
-usage() { echo "usage: $ME [-f|--force] [-v|--verbose] <OID..."; exit 1; }
+usage() { echo "usage: $ME [-f|--force] <OID..."; exit 1; }
 IS_FORCE=false
-IS_QUIET=true
 while test $# -ne 0 ; do
     case $1 in
         -f|--force) shift ; IS_FORCE=true ;;
-        -v|--verbose) shift ; IS_QUIET=false ;;
         *) usage ;;
     esac
 done
 test $# -eq 0 || usage
 sed 's/  */\n/g' | \
 while read OID ; do
-    echo -n "$KEEPDB/spot/"
+    echo -n $OID
+    echo -n " $KEEPDB/spot/"
     echo $OID | tr '/' '\n' | sed '1s/./&\//g' | tr -d '\n'
     echo
-done | while read PREFIX ; do
+done | while read OID PREFIX ; do
     LABEL_FILE="${PREFIX}.label"
     FILENAME=$(sed -e '/^filename:/!d' -e 's/^[^:]*://' "$LABEL_FILE")
-    test -n "$FILENAME" || fail "missing 'filename:...' in $(basename $LABEL_FILE)"
-    $IS_FORCE || { test -e "$FILENAME" && fail "filename '$FILENAME' already exists" || : ; }
-    $IS_QUIET || echo -n $(basename "$PREFIX")\ --\>\  >&2
+    test -n "$FILENAME" || fail "missing 'filename:...' label in $OID"
+    test ! -e "$FILENAME" || {
+        $IS_FORCE || fail "filename '$FILENAME' already exists -- ${OID}"
+        echo "${ME}: warning: overwriting '$FILENAME' ${OID}" >&2
+    }
     gzip -cd "${PREFIX}.gz" >$FILENAME
-    $IS_QUIET || echo $FILENAME >&2
 done
