a8de9b672b8c05a4cb6521477f77597b8b8ff209
Rafael Lorandi <coolparadox@gmail.com>
2023-07-07 17:05:22 +0100

    refrain from removing the spot directory

--- src/forget.sh
+++ src/forget.sh
@@ -12,13 +12,14 @@ __eod__
     exit 1
 }
 test $# -eq 0 || usage
+SPOT_DIR="$KEEPDB/spot"
 sed 's/  */\n/g' | \
 while read OID ; do
-    echo -n "$KEEPDB/spot/"
     tr '/' '\n' <<<$OID | sed '1s/./&\//g' | tr -d '\n'
     echo
-done | while read PREFIX ; do
-    PREFIX_DIR=$(dirname "$PREFIX")
-    find "$PREFIX_DIR" -maxdepth 1 -name $(basename "$PREFIX"\*) -delete
-    rmdir -p --ignore-fail-on-non-empty "$PREFIX_DIR"
+done | while read OID_BASE ; do
+    OID_BASE_SUBDIR=$(dirname "$OID_BASE")
+    OID_SUBDIR="$SPOT_DIR/$OID_BASE_SUBDIR"
+    find "$OID_SUBDIR" -maxdepth 1 -name $(basename "$OID_BASE"\*) -delete
+    ( cd "$SPOT_DIR" && rmdir -p --ignore-fail-on-non-empty "$OID_BASE_SUBDIR" )
 done
