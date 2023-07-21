c41ca31a9ecf9276da8abc9094b43914c38ccd50
Rafael Lorandi <coolparadox@gmail.com>
2023-07-21 12:05:55 +0100

    recover --skip

--- src/recover.sh
+++ src/recover.sh
@@ -4,13 +4,15 @@ ME=$(basename $0)
 fail() { echo "${ME}: error: $*" >&2 ; exit 1 ; }
 test -v KEEPDB || fail "missing KEEPDB environment variable"
 test -d "$KEEPDB" || fail "missing KEEPDB directory '$KEEPDB'"
-usage() { echo "usage: $ME [-C|--dir DIR] [-f|--force] <<<OID..."; exit 1; }
+usage() { echo "usage: $ME [-C|--dir DIR] [-f|--force] [-s|--skip] <<<OID..."; exit 1; }
 IS_FORCE=false
+IS_SKIP=false
 RECOVER_DIR=$PWD
 while test $# -ne 0 ; do
     case $1 in
         -C|--dir) shift ; test $# -ne 0 || usage ; RECOVER_DIR=$1 ; shift ;;
         -f|--force) shift ; IS_FORCE=true ;;
+        -s|--skip) shift ; IS_SKIP=true ;;
         *) usage ;;
     esac
 done
@@ -27,8 +29,14 @@ done | while read OID PREFIX ; do
     FILENAME=$(sed -e '/^filename:/!d' -e 's/^[^:]*://' "$LABEL_FILE")
     test -n "$FILENAME" || fail "missing 'filename:...' label in $OID"
     test ! -e "$FILENAME" || {
-        $IS_FORCE || fail "filename '$FILENAME' already exists in '$RECOVER_DIR' -- ${OID}"
-        echo "${ME}: warning: overwriting '$FILENAME' in '$RECOVER_DIR' due to ${OID}" >&2
+        if $IS_SKIP ; then
+            echo "${ME}: info: skipping '$FILENAME' in '$RECOVER_DIR' due to ${OID}: file exists" >&2
+            continue
+        elif $IS_FORCE ; then
+            echo "${ME}: warning: overwriting '$FILENAME' in '$RECOVER_DIR' due to ${OID}" >&2
+        else
+            fail "filename '$FILENAME' already exists in '$RECOVER_DIR' -- ${OID}"
+        fi
     }
-    gzip -cd "${PREFIX}.gz" >$FILENAME
+    gzip -cd "${PREFIX}.gz" >"$FILENAME"
 done
